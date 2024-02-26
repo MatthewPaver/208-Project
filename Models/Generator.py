67409a3c0bc1ef017c49b6c7f201a09d025e9f49
Ash23733 <stephen.bromley04@gmail.com>
2024-02-26 16:07:07 +0000

    Added basic method stubs and imported torch.nn module for both types of model

---
+++ Models/Generator.py
@@ -0,0 +1,10 @@
+import torch.nn as nn
+
+class Generator(nn.Module):
+    def __init__(self):
+        #TODO: Define all layers of the network
+        return
+
+    def forward(self, input):
+        #TODO: Define how the input passes through the layers
+        return
