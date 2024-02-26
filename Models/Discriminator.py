c96a28b34be561618b40519e26a1ac0dea0fc5d4
Ash23733 <stephen.bromley04@gmail.com>
2024-02-26 16:07:07 +0000

    Added basic method stubs and imported torch.nn module for both types of model

---
+++ Models/Discriminator.py
@@ -0,0 +1,11 @@
+import torch.nn as nn
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        #TODO: Define all layers of the network
+        return
+
+    def forward(self, input):
+        #TODO: Define how the input passes through the layers
+        return
---
+++ Models/Generator.py
@@ -0,0 +1,11 @@
+import torch.nn as nn
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        #TODO: Define all layers of the network
+        return
+
+    def forward(self, input):
+        #TODO: Define how the input passes through the layers
+        return
