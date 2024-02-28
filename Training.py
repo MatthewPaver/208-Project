66068d86032eb863d66deb11326dea4355b21ab1
Wilfmorlidge <124621797+Wilfmorlidge@users.noreply.github.com>
2024-02-28 12:07:16 +0000

    Update Training.py

--- Training.py
+++ Training.py
@@ -12,13 +12,6 @@ def train() -> None:
     #TODO: Implement training loops to call train_one_epoch and do any needed setup
     return
 
-def eval() -> float:
-    #TODO: Implement a version of inception score to objectively determine performance of generator
-    return
-
-def weights_init(model: Generator) -> None:
-    #TODO: Initialise the weights of the generator before training begins
-    return
 
 if __name__ == "__main__":
     print("hi")
