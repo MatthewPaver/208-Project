c6e4671ca0753d26a4338b0c342e7124021c2d62
Wilfmorlidge <124621797+Wilfmorlidge@users.noreply.github.com>
2024-02-29 10:51:10 +0000

    Update Training.py

--- Training.py
+++ Training.py
@@ -8,12 +8,13 @@ def train_one_epoch() -> None:
     #TODO: Implement training for both nets for one epoch
     return
 
-def train() -> None:
+def train(dataset,5) -> None:
     #TODO: Implement training loops to call train_one_epoch and do any needed setup
     return
 
 
 if __name__ == "__main__":
     print("hi")
+    train(ds,5)
     #TODO: Call methods to setup and begin training. This is equivilant of the main method in java
     #If statement used to dictate only main thread can execute not worker threads
