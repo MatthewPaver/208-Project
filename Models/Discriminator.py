a4087067a4659765167f9340ca92720b7c6d59d4
Wilfmorlidge <124621797+Wilfmorlidge@users.noreply.github.com>
2024-02-29 10:44:41 +0000

    Update Discriminator.py

--- Models/Discriminator.py
+++ Models/Discriminator.py
@@ -24,17 +24,20 @@ from matplotlib import gridspec
         #TODO: Define all layers of the network
         #return
 
-def image_preprocessor(generator_output+real):
-    x = layers.Input(generator_output+real)
-    return x #this is subject to change as I research how training works in tensorflow
+def image_preprocessor(image_shape = (128,128,3)):
+    x = layers.Input(shape=image_shape)
+    return x # when calling the model from the training loop, images and labels are passed automatically and do not need to be referenced in function calls.
 
-def label_preprocessor(label_vector):
-    x = layers.Input(label_vector)
-    return x #this is subject to change as I research how training works in tensorflow
+def label_preprocessor():
+    x = layers.Input(shape=(1,)) # in order for keras concatenation layer to work, both inputs must have the same dimensionality
+    x = layers.Embedding(3,50)(x) # an embedding layer converts the declarative input into a tensor (so it can be reshaped) 50 refers to the size of the output, 3 is the input 
+    # dimension, and I frankly don't know what that is (but three seems the standard value).
+    x = layers.Dense((128,128,3)) (x) # 128,128,3 is the shape of the image input using the tensorflow rock-paper-scissors dataset 
+    return x # in order for keras concatenation layer to work, both inputs must have the same dimensionality
 
-def create_discriminator():
-    stream1_input = image_preprocessor()
-    stream2_input = label_preprocessor()
+def create_discriminator(): # in keras forward propogation is handled automatically when one calls 'discriminator' with input data
+    stream1_input = image_preprocessor() #keras simply assigns the first dimension of the input tensor (when the model is called) to the first input layer it encounters, and so on.
+    stream2_input = label_preprocessor() # it is therefore very important that input is passed image,label and not label,image.
     x = layers.Concatenate() ([stream1_input,stream2_input])
     
     x = layers.Conv2D(64,4) (x)
