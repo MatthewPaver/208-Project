82388e2903476ba937ba3f0bbe77aa86666d02c1
flashdash101 <adesina0202@gmail.com>
2024-03-01 03:37:33 +0000

    Changed up a few things, made sure each layer was done properly

--- Models/Generator.py
+++ Models/Generator.py
@@ -6,22 +6,32 @@ def build_generator(latent_dim):
     inputs = layers.Input(shape=(latent_dim,))
 
     x = layers.Dense(8*8*128)(inputs)
-    x = layers.BatchNormalization()
+    x = layers.BatchNormalization()(x)
+    x = layers.ReLU()(x)
     x = layers.Reshape((8, 8, 128))(x)
+    
+    #Activation function will be Sigomid and ReLU (Can change to Leaky ReLU if required)
+    #Strides are doubling the input size
+    #Batch normalization to ensure smooth training
     
     
     x = layers.Conv2DTranspose(128, kernel_size=4, strides=2, padding='same')(x)
-    x = layers.BatchNormalization()
+    #16 x 16
+    x = layers.BatchNormalization()(x)
     x = layers.ReLU()(x)
    
     
     x = layers.Conv2DTranspose(256, kernel_size=4, strides=2, padding='same')(x)
-    x = layers.BatchNormalization()
+    #32 x 32
+    x = layers.BatchNormalization()(x)
+    x = layers.ReLU()(x)
+
+    x = layers.Conv2DTranspose(512, kernel_size=4, strides=2, padding='same')(x)
+    #64 x 64
+    x = layers.BatchNormalization()(x)
     x = layers.ReLU()(x)
     
-    x = layers.Conv2DTranspose(512, kernel_size=4, strides=2, padding='same')(x)
-    x = layers.BatchNormalization()
-    x = layers.ReLU()(x)
+    
    
     
     
@@ -39,4 +49,3 @@ generator = build_generator(latent_dim)
 # Summary of the generator model
 generator.summary()
 
-#Tesing github push from vscode
