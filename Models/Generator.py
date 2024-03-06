3a654358d09142ab41f013d1d4a624e39e93bf97
Wilfmorlidge <124621797+Wilfmorlidge@users.noreply.github.com>
2024-03-06 15:26:45 +0000

    Update Generator.py

--- Models/Generator.py
+++ Models/Generator.py
@@ -23,39 +23,46 @@ def tag_preprocessing() -> tf.Tensor:
 
 
 def build_generator(latent_dim): 
-    input_stream1 = image_preprocessing(latent_dim)
-    input_stream2 = tag_preprocessing()
-
-    x = layers.Concatenate() ([input_stream2,input_stream1])
+input_stream2 = tag_preprocessing()
+    input_stream1 = image_preprocessing()
+    x = layers.Concatenate() ([input_stream1,input_stream2])
     
     #Activation function will be Tanh and ELU (Can change to Leaky ReLU/ReLU after further experiments)
     #Strides are doubling the input size
     #Batch normalization to ensure smooth training
     #Tanh is typically better and outputs to [-1, 1] but using a normlized tanh function it outputs [0,1] instead
     
-    x = layers.Conv2DTranspose(128, kernel_size=4, strides=2, padding='same')(x)
-    #16 x 16
+    x = layers.Conv2DTranspose(64*16, kernel_size=4, strides=2, padding='same')(x)
+    #8 x 8
     x = layers.BatchNormalization()(x)
     x = layers.ELU()(x)
    
     
-    x = layers.Conv2DTranspose(256, kernel_size=4, strides=2, padding='same')(x)
+    x = layers.Conv2DTranspose(64*8, kernel_size=4, strides=2, padding='same')(x)
+    #16 x 16
+    x = layers.BatchNormalization()(x)
+    x = layers.ELU()(x)
+
+    x = layers.Conv2DTranspose(64*4, kernel_size=4, strides=2, padding='same')(x)
     #32 x 32
     x = layers.BatchNormalization()(x)
     x = layers.ELU()(x)
 
-    x = layers.Conv2DTranspose(512, kernel_size=4, strides=2, padding='same')(x)
+    x = layers.Conv2DTranspose(64*2, kernel_size=4, strides=2, padding='same')(x)
     #64 x 64
+    x = layers.BatchNormalization()(x)
+    x = layers.ELU()(x)
+
+    x = layers.Conv2DTranspose(64*1, kernel_size=4, strides=2, padding='same')(x)
+    #128 x 128
     x = layers.BatchNormalization()(x)
     x = layers.ELU()(x)
     
     
-   
-    
-    
     outputs = layers.Conv2D(3, kernel_size=4, padding='same', activation=normalized_tanh)(x)
-
-    model = tf.keras.Model(inputs=inputs, outputs=outputs, name='generator')
+    
+    
+    model = tf.keras.Model(inputs=[inputs1,inputs2], outputs=outputs, name='generator')
     return model
 
 # Define latent dimension
