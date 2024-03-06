cd7f94ea42770fa47ede6e1448086a969f88fa88
Wilfmorlidge <124621797+Wilfmorlidge@users.noreply.github.com>
2024-03-06 12:40:13 +0000

    Update Training.py

--- Training.py
+++ Training.py
@@ -12,8 +12,10 @@ import time
 import tensorflow_datasets as tfds
 from matplotlib import gridspec
 
+learning_rate = 0.1
+
 # this instantiates the optimizer for the generator, it is outside of the training loop because it only needs to run once, and takes a lot of time.
-generator_optimizer = tf.keras.optimizers.Adam(lr = 0.0002, beta_1 = 0.5, beta_2 = 0.999 )
+generator_optimizer = tf.keras.optimizers.Adam(lr = learning_rate, beta_1 = 0.5, beta_2 = 0.999 )
 
 #Method parameters not stubbed as dependent on implementation
 #image input may need normalising to (128,128,3) as part of the dataloader
@@ -23,6 +25,7 @@ def train_one_epoch(dataset: Dataset) -> None:
         random_image_noise = tf.convert_to_tensor(random_image_noise, dtype=tf.float32)
         fake_images = generator([random_image_noise,labels])
         discriminator.compile('Adam','binary_crossentropy')
+        tf.keras.set_value(discriminator.optimizer.learning_rate, learning_rate)
         discriminator.fit((batch,labels),np.ones(128,),128,1)
         discriminator.fit((fake_images,labels),np.zeros(128,),128,1)
         # the generator has a non standard loss function (using the loss of another model to update)
