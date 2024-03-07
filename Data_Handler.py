049fb9bcd86537831db6ff94884f35e9993ea43f
Ash23733 <stephen.bromley04@gmail.com>
2024-03-07 23:43:35 +0000

    Attempted to switch back to rock paper scissors. Needs more work

--- Data_Handler.py
+++ Data_Handler.py
@@ -1,6 +1,8 @@
 from tensorflow.keras import datasets
+import tensorflow_datasets as tfds
+import tensorflow as tf
 
-IMAGE_DIMENSIONS = 128
+IMAGE_DIMENSIONS = (128,128)
 
 def load_dataset():
     """
@@ -8,7 +10,12 @@ def load_dataset():
 
     :return: A tuple containing images and labels. Order -> images, labels
     """
-    train, _ = datasets.fashion_mnist.load_data()
-    x, y = train
-    x = x/255
-    return x, y
+    ds = tfds.load('RockPaperScissors', split='train', as_supervised=True, shuffle_files=True)
+    images = []
+    labels = []
+    for x,y in ds:
+        x = tf.image.resize(x, IMAGE_DIMENSIONS)
+        x = x/255
+        images.append(x)
+        labels.append(y)
+    return images, labels
