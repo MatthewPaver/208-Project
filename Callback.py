d20c23bf24b56b9f0974a64e5e4e679114b433c5
Ash23733 <stephen.bromley04@gmail.com>
2024-03-13 23:00:30 +0000

    Implemented custom callback and tuner to save model weights in .h5 files and optimiser states in pickle dumps. It also reloads upon restart

--- Callback.py
+++ Callback.py
@@ -1,3 +1,28 @@
 from keras import callbacks
+import os
+import pickle
 
 class MyCallback(callbacks.Callback):
+    def __init__(self, save_directory):
+        super().__init__()
+        self.save_directory = save_directory
+
+    def on_epoch_end(self, epoch, logs=None):
+        epoch = epoch + 1
+        generator_filename = os.path.join(self.save_directory, f"generator_epoch_{epoch}.h5")
+        discriminator_filename = os.path.join(self.save_directory, f"discriminator_epoch_{epoch}.h5")
+        generator_optimizer_filename = os.path.join(self.save_directory, f"/generator_optimizer_epoch_{epoch}.pkl")
+        discriminator_optimizer_filename = os.path.join(self.save_directory, f"discriminator_optimizer_epoch_{epoch}.pkl")
+
+        os.makedirs(self.save_directory, exist_ok=True)
+
+        self.model.generator.save_weights(generator_filename)
+        self.model.discriminator.save_weights(discriminator_filename)
+
+        with open(generator_optimizer_filename, 'wb') as f:
+            pickle.dump(self.model.generator_optimizer.get_config(), f)
+
+        with open(discriminator_optimizer_filename, 'wb') as f:
+            pickle.dump(self.model.discriminator_optimizer.get_config(), f)
+
+        print(f"Models and optimizers saved for epoch {epoch}.")
--- Tuner.py
+++ Tuner.py
@@ -2,6 +2,11 @@ from keras_tuner import tuners
 from keras_tuner.src.engine import tuner_utils
 import copy
 import Callback
+import os
+import pickle
+from tensorflow.keras.optimizers import Adam
+import json
+import collections
 
 class MyTuner(tuners.GridSearch):
     def __init__(self,
@@ -27,29 +32,44 @@ class MyTuner(tuners.GridSearch):
                                       **kwargs)
         self.reloaded = False
 
-    def reload(self):
-        self.reloaded = True
-        super(MyTuner, self).reload()
-
-    def save(self):
-        super(MyTuner, self).save()
-
-
-#TODO: - finish implementing run trial and custom callback
     def run_trial(self, trial, *args, **kwargs):
         """
-        Overwrites the superclass run_trial to allow reloading mid-trial
+        Overwritten superclass method to allow reloading each epoch
 
         Majority of code copied from superclass with some modifications to allow models
         to be reloaded after a crash or termination. The model is loaded with all the
-        weights and gradients of the last completed epoch.
+        weights and gradients of the last completed epoch. Model weights are loaded
+        from the .h5 files and optimiser states are loaded from the pickle dumps
 
         :param trial:
         :param args:
         :param kwargs:
         :return:
         """
-        model_checkpoint = Callback.MyCallback()
+        hp = trial.hyperparameters
+        model = self._try_build(hp)
+        save_directory = os.path.join(self.get_trial_dir(trial.trial_id), "/saves")
+        if self.reloaded:
+            epoch = self.find_latest_epoch(save_directory)
+
+            with open("saves/generator_optimizer_epoch_0.pkl", 'rb') as f:
+                generator_optimizer_config = pickle.load(f)
+            model.generator_optimizer = Adam(**generator_optimizer_config)
+
+            with open("saves/discriminator_optimizer_epoch_0.pkl", 'rb') as f:
+                discriminator_optimizer_config = pickle.load(f)
+            model.discriminator_optimizer = Adam(**discriminator_optimizer_config)
+
+            model.generator.load_weights("saves/generator_epoch_0.h5")
+            model.discriminator.load_weights("saves/discriminator_epoch_0.h5")
+
+            kwargs['epochs'] -= epoch
+            print("Loaded model weights")
+            print(f"Resuming at the end of epoch: {epoch}")
+            print(f"Remaining epochs: {kwargs['epochs']}")
+            self.reloaded = False
+
+        model_checkpoint = Callback.MyCallback(save_directory)
         original_callbacks = kwargs.pop("callbacks", [])
         copied_kwargs = copy.copy(kwargs)
         callbacks = self._deepcopy_callbacks(original_callbacks)
@@ -57,5 +77,51 @@ class MyTuner(tuners.GridSearch):
         self._configure_tensorboard_dir(callbacks, trial, 0)
         callbacks.append(model_checkpoint)
         copied_kwargs["callbacks"] = callbacks
-        results = self._build_and_fit_model(trial, *args, **copied_kwargs)
+        results = self.hypermodel.fit(hp, model, *args, **kwargs)
         return results
+
+    def find_latest_epoch(self, save_directory):
+        epochs_seen = []
+        for file in os.listdir(save_directory):
+            if file.startswith("generator_epoch_"):
+                epochs_seen =  int(file.split("_")[2].split(".")[0])
+        if epochs_seen:
+            return max(epochs_seen)
+        else:
+            return 0
+
+    def reload(self):
+        self.reloaded = True
+        directory = os.path.join(self.project_dir, "linked_list_data.json", )
+        with open(directory, "r") as file:
+            loaded_data = json.load(file)
+        self.from_dict(loaded_data["_ordered_ids"])
+        self.oracle._populate_next = loaded_data["_populate_next"]
+        super(MyTuner, self).reload()
+
+    def save(self):
+        data = {
+            '_ordered_ids': self.to_dict(),
+            '_populate_next': self.oracle._populate_next
+        }
+        directory = os.path.join(self.project_dir, "linked_list_data.json",)
+        with open(directory, "w") as file:
+            json.dump(data, file)
+        super(MyTuner, self).save()
+        return
+
+    def to_dict(self):
+        linked_list = self.oracle._ordered_ids
+        next_index_dict = dict(linked_list._next_index)
+        return {
+            "_memory":linked_list._memory,
+            "_data_to_index": linked_list._data_to_index,
+            "_next_index": next_index_dict,
+            "_last_index": linked_list._last_index
+        }
+
+    def from_dict(self, data):
+        self.oracle._ordered_ids._memory= data["_memory"]
+        self.oracle._ordered_ids._data_to_index = data["_data_to_index"]
+        self.oracle._ordered_ids._next_index = collections.defaultdict(lambda: None, data["_next_index"])
+        self.oracle._ordered_ids._last_index = data["_last_index"]
