30d39fe4cf332e3620110774f7c53926032cb3cc
Ash23733 <stephen.bromley04@gmail.com>
2024-03-21 12:59:52 +0000

    Updated task setter to handle automation of task setting

--- Task_Setter.py
+++ Task_Setter.py
@@ -1,15 +1,37 @@
 import pika
 import json
+from itertools import product
 
-connection = pika.BlockingConnection(pika.URLParameters('amqps://bfjzexuw:h91qsaFYNrHc8Ag_5WVOOVdFH2MpnOby@whale.rmq.cloudamqp.com/bfjzexuw'))
-channel = connection.channel()
+FILE_PATH = "AllTasks.json"
 
-channel.queue_declare(queue='Tuning', durable=True)
+def set_tasks():
+    connection = pika.BlockingConnection(pika.URLParameters('amqps://bfjzexuw:h91qsaFYNrHc8Ag_5WVOOVdFH2MpnOby@whale.rmq.cloudamqp.com/bfjzexuw'))
+    channel = connection.channel()
 
-hp = {'Generator LR': 0.1, 'Discriminator LR': 0.1, "Batch Size": 128, "Latent Dim": 100, "trial_id": "Trial_00"}
-message = json.dumps(hp)
+    channel.queue_declare(queue='Tuning', durable=True)
 
-channel.basic_publish(exchange='', routing_key='Tuning', body=str(message), properties=pika.BasicProperties(delivery_mode=2))
-print("Sent task:", message)
+    with open(FILE_PATH, "r") as file:
+        tasks_to_be_set = json.load(file)
 
-connection.close()
+    for i in tasks_to_be_set:
+        message = json.dumps(i)
+        channel.basic_publish(exchange='', routing_key='Tuning', body=str(message),properties=pika.BasicProperties(delivery_mode=2))
+        print("Sent task:", message)
+
+    connection.close()
+
+def create_tasks():
+    lr = [1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6]
+    batch_size = [32, 64, 128, 256]
+    latent_dim = [100]
+    id = 0
+    combinations = product(lr, lr, batch_size, latent_dim)
+    list_of_trials = []
+    for i in combinations:
+        trial = {'Generator LR': i[0], 'Discriminator LR': i[1], "Batch Size": i[2], "Latent Dim": i[3], "trial_id": id}
+        list_of_trials.append(trial)
+        id += 1
+    with open(FILE_PATH, "w") as file:
+        json.dump(list_of_trials, file)
+
+create_tasks()
