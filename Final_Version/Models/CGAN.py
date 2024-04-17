073ceb6142611dd3b31837bd1e55fb503cbfad1c
Ash23733 <stephen.bromley04@gmail.com>
2024-04-17 13:12:22 +0100

    Updated the training loop

--- Final_Version/Models/CGAN.py
+++ Final_Version/Models/CGAN.py
@@ -3,28 +3,36 @@ Contains the CGAN model class
 
 This module defines how a CGAN will function with the 2 different models composing it
 as well as a custom training process along with custom loss functions and metric
-reporting. generator_loss and discriminator_loss are functions but shouldn't be
-used outside this file while the CGAN class itself contains init, compile and a
-dynamic property called metrics that can be used by other code
+reporting.The CGAN class itself contains init, compile and a dynamic property
+called metrics that can be used by other code
 """
 
-from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Optimizer
 import tensorflow as tf
 from keras import metrics
-import datetime
-
-logdir = "logs/scalars/9"
-file_writer = tf.summary.create_file_writer(logdir + "/metrics")
-file_writer.set_as_default()
 
 
-def add_instance_noise(x, batch_size):
-    noise = tf.random.normal(batch_size, mean=0.5, stddev=0.05)
-    return x + noise
+def gp(real_images, fake_images, discriminator, labels):
+    batch_size = tf.shape(real_images)[0]
 
-class CGAN(Model):
-    def __init__(self, generator: Model, discriminator: Model) -> None:
+    alpha = tf.random.uniform(shape=[batch_size, 1, 1, 1], minval=0, maxval=1)
+
+    interpolated_images = alpha * real_images + ((1 - alpha) * fake_images)
+
+    with tf.GradientTape() as tape:
+        tape.watch(interpolated_images)
+        logits = discriminator([interpolated_images, labels], training=True)
+
+    gradients = tape.gradient(logits, interpolated_images)
+
+    gradients_norm = tf.sqrt(tf.reduce_sum(tf.square(gradients), axis=[1, 2, 3]))
+
+    gradient_penalty = tf.reduce_mean((gradients_norm - 1.0) ** 2)
+
+    return gradient_penalty
+
+
+class CGAN():
+    def __init__(self, generator, discriminator):
         """
         Instantiates an instance of a CGAN
 
@@ -37,91 +45,70 @@ class CGAN(Model):
         self.discriminator = discriminator
         self.d_optimiser = None
         self.g_optimiser = None
-        self.d_loss = metrics.Mean(name="Discriminator Loss")
-        self.g_loss = metrics.Mean(name="Generator Loss")
-        self.batches = 0
+        self.gradient_penalty = metrics.Mean()
+        self.discriminator_loss = metrics.Metric()
+        self.generator_loss = metrics.Metric()
 
-    def compile(self, gen_optimiser: Optimizer, disc_optimiser: Optimizer) -> None:
+    def compile(self, gen_optimiser, disc_optimiser):
         """
-        Adds optimisers for both models
+        Adds the optimisers to the two models
 
-        Overwrites the compile method from tf.keras.Model superclass. Saves optimisers
-        as attributes to use later. Need to call this method before training or tuning
-
-        :param gen_optimiser: A keras optimiser to use for the generator
-        :param disc_optimiser: A keras optimiser to use for the discriminator
+        :param gen_optimiser: The optimiser for the generator
+        :param disc_optimiser: The optimiser for the discriminator
         """
-
-        super(CGAN, self).compile()
+        super().compile()
         self.g_optimiser = gen_optimiser
         self.d_optimiser = disc_optimiser
 
-    def gp(self, batch_size, real_images, fake_images, labels):
-        alpha = tf.random.uniform([batch_size, 1, 1, 1], 0.0, 1.0)
-        interpolated = alpha * real_images + (1 - alpha) * fake_images
-        with tf.GradientTape() as gp_tape:
-            gp_tape.watch(interpolated)
-            pred = self.discriminator([interpolated, labels], training=True)
-        grads = gp_tape.gradient(pred, interpolated)
-        norm = tf.sqrt(tf.reduce_sum(tf.square(grads), axis=[1, 2, 3]))
-        gp = tf.reduce_mean((norm - 1.0) ** 2)
-        return gp
-
     @tf.function
     def train_step(self, data):
-        images, labels = data
-        batch_size = tf.shape(images)[0]
-        latent_dim = 100
+        """
+        Defines the training process of a single batch. The discriminator is updated
+        5 times for each batch then the generator is updated once
 
-        for _ in range(10):
-            noise = tf.random.normal([batch_size, latent_dim])
-            with tf.GradientTape() as disc_tape:
-                generated_images = self.generator([noise, labels])
+        :param data: The batch of images and labels
+        :return: The result of the training
+        """
+        real_samples, labels = data
+        batch_size = tf.shape(real_samples)[0]
+        noise = tf.random.normal([batch_size, 128])
+        gps = []
 
-                real_output = self.discriminator([images, labels], training=True)
-                fake_output = self.discriminator([generated_images, labels], training=True)
+        for _ in range(5):
+            with tf.GradientTape() as tape:
+                fake_samples = self.generator([noise, labels], training=True)
+                pred_real = self.discriminator([real_samples, labels], training=True)
+                pred_fake = self.discriminator([fake_samples, labels], training=True)
 
-                real_loss = tf.reduce_mean(real_output)
-                fake_loss = tf.reduce_mean(fake_output)
-                gp = self.gp(batch_size, images, generated_images, labels)
-                disc_loss = (-(real_loss - fake_loss) + (gp * 20))
-                tf.summary.scalar('fake_loss', data=fake_loss, step=self.batches)
-                tf.summary.scalar('real_loss', data=real_loss, step=self.batches)
-                tf.summary.scalar('gp', data=gp, step=self.batches)
+                gp = gp(real_samples, fake_samples, self.discriminator, labels)
+                gps.append(gp)
 
-                self.batches += 1
-            gradients_of_disc = disc_tape.gradient(disc_loss, self.discriminator.trainable_variables)
-            self.d_optimiser.apply_gradients(zip(gradients_of_disc, self.discriminator.trainable_variables))
-            for i, grad in enumerate(gradients_of_disc):
-                tf.summary.histogram(f'gradient_{i}', grad, step=self.batches)
-            b_size = tf.cast(batch_size, dtype=tf.float32)
-            disc_loss = disc_loss / b_size
-            tf.summary.scalar('Disc loss', data=disc_loss, step=self.batches)
+                real_loss = tf.reduce_mean(pred_real)
+                fake_loss = tf.reduce_mean(pred_fake)
+                disc_loss = (fake_loss - real_loss) + (gp * 10)
 
-        noise = tf.random.normal([batch_size, latent_dim])
-        with tf.GradientTape() as gen_tape:
-            generated_images = self.generator([noise, labels], training=True)
-            fo = self.discriminator([generated_images, labels], training=True)
-            gen_loss = -tf.reduce_mean(fo)
-        gradients_of_gen = gen_tape.gradient(gen_loss, self.generator.trainable_variables)
-        self.g_optimiser.apply_gradients(zip(gradients_of_gen, self.generator.trainable_variables))
+            grads = tape.gradient(disc_loss, self.discriminator.trainable_variables)
 
-        gen_loss = gen_loss / b_size
-        self.d_loss.update_state(disc_loss)
-        self.g_loss.update_state(gen_loss)
+            self.d_optimiser.apply_gradients(zip(grads, self.discriminator.trainable_variables))
 
-        tf.summary.scalar('Gen Loss', data=gen_loss, step=self.batches)
-        return {"Generator Loss": self.g_loss.result(),
-                "Discriminator Loss": self.d_loss.result()
-                }
+        with tf.GradientTape() as tape:
+            fake_samples = self.generator([noise, labels], training=True)
+            pred_fake = self.discriminator([fake_samples, labels], training=True)
+            gen_loss = -tf.reduce_mean(pred_fake)
 
+        grads = tape.gradient(gen_loss, self.generator.trainable_variables)
 
+        self.g_optimiser.apply_gradients(zip(grads, self.generator.trainable_variables))
+
+        avg_gp = tf.reduce_mean(gps)
+        self.gradient_penalty.update_state(avg_gp)
+        self.discriminator_loss.update_state(disc_loss)
+        self.generator_loss.update_state(gen_loss)
+
+        return {"discriminator_loss": self.discriminator_loss.result(),
+                "generator_loss": self.generator_loss.result(),
+                "gradient_penalty": self.gradient_penalty.result()}
 
     @property
     def metrics(self):
-        """
-        A property dynamically set to the loss values. Needed to prevent error at end of epoch
-        
-        :return: Tuple of discriminator loss and generator loss
-        """
-        return [self.d_loss, self.g_loss]
+        return [self.discriminator_loss, self.generator_loss, self.gradient_penalty]
